0040c6a55b47cb7d0f5ecd10fce9db3dc896411e
youphendriks <youp@youphendriks>
2024-06-19 10:27:16 +0200

    Comments without markdown syntax

--- scripts/create-readme.py
+++ scripts/create-readme.py
@@ -3,22 +3,50 @@ import os
 import json
 from py_markdown_table.markdown_table import markdown_table
 
+taglist = [
+    "Title",
+    "Statement",
+    "Description",
+    "License",
+    "Dependencies",
+    "Code language",
+    "Available platform",
+    "Interface CLI",
+    "Interface GUI",
+    "Interface web platform",
+    "Input format",
+    "Output format",
+    "Source code",
+    "Documentation link",
+    "Installation instructions",
+    "Zenodo link",
+    "Citation instructions"
+]
+
 def importProjectList(path):
     with open(path) as f:
         projectlist = json.load(f)
     for project in projectlist:
         name = project["name"]
         rawlink = project["rawlink"]
-        os.system("mkdir -p ../db/" + name)
-        os.system("wget -O ../db/" + name + "/README.md " + rawlink)
+        os.system("mkdir -p db/" + name)
+        os.system("wget -O db/" + name + "/README.md " + rawlink)
     return projectlist
 
 def extractData(projectlist):
     for project in projectlist:
         name = project["name"]
-        projectlib = open(("../db/" + name + "/README.md"), 'r').read()
-        split1 = projectlib.split('<!--Statement-->')
-        print(split1)
+        projectlib = open(("db/" + name + "/README.md"), 'r').read()
+        tagdict = extractTags(projectlib, taglist)
+
+def extractTags(projectlib, taglist):
+    tagdict = {}
+    for tag in taglist:
+        split1 = projectlib.split("<!--"+tag+"-->")
+        split2 = split1[1].split("<!--/"+tag+"-->")
+        tagdict[tag]=split2[0]
+    print(tagdict)
+    return tagdict
 
 #def createJson(dblist):
 
@@ -28,7 +56,5 @@ def extractData(projectlist):
 
 #def saveMarkdownTable(dbmarkdown):
 
-
-
-projectlist = importProjectList("../db/project_list.json")
+projectlist = importProjectList("db/project_list.json")
 extractData(projectlist)
