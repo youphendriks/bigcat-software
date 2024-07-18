56d69e1916cebc980574f6e521cf894f9f054e89
youphendriks <youp@youphendriks>
2024-07-18 17:22:26 +0200

    Update

--- db/BridgeDB/README.md
+++ db/BridgeDB/README.md
@@ -1,6 +1,16 @@
-# <!--Title-->{{Replace by title}}<!--/Title-->  
-## **<!--Statement-->{{Replace by short 1 sentence statement describing the project.}}<!--/Statement-->**  
-<!--Description-->{{Replace by description of the project (max 150 words)}}<!--/Description-->  
+# <!--Title-->CyNeo4j<!--/Title-->  
+## **<!--Statement-->A Cytoscape app to connect to a Neo4j database and execute extensions of the Neo4j database. This app has been extended to query the shortest path for metabolic conversions.<!--/Statement-->**  
+<!--Description-->Minimum System Requirements (for CyNeo4j 2.x):
+
+Neo4j 3.5.x (requires Java 8 but can run with Java 11 as well)
+Cytoscape 3.9.x (automatically comes with a Java 11 download)
+Java 11 (Eclipse)
+Compiling the script The setup of this project in Eclipse has been tested with:
+
+Eclipse IDE 2022-06 (Java and Web developers), OS Linux (Ubuntu 18.04.6 LTS), JRE JavaSE-11.
+Find the app in the Cytoscape app store (http://apps.cytoscape.org/apps/cyneo4j) and on Github (https://github.com/cyneo4j/cyNeo4j).
+
+For a tutorial and installation guide for the server side component checkout https://cyneo4j.github.io/DSMN/ !<!--/Description-->  
 **License:**<!--License-->{{Replace by license title}}<!--/License-->  
 **Dependencies:**<!--Dependencies-->{{Replace by needed dependencies}}<!--/Dependencies-->  
 **Code language:**<!--Code language-->{{Replace by }}<!--/Code language-->  
--- scripts/create-readme.py
+++ scripts/create-readme.py
@@ -3,27 +3,7 @@ import os
 import json
 from py_markdown_table.markdown_table import markdown_table
 
-taglist = [
-    "Title",
-    "Statement",
-    "Description",
-    "License",
-    "Dependencies",
-    "Code language",
-    "Available platform",
-    "Interface CLI",
-    "Interface GUI",
-    "Interface web platform",
-    "Input format",
-    "Output format",
-    "Source code",
-    "Documentation link",
-    "Installation instructions",
-    "Zenodo link",
-    "Citation instructions"
-]
-
-def importProjectList(path):
+def importProjectlist(path):
     with open(path) as f:
         projectlist = json.load(f)
     for project in projectlist:
@@ -34,28 +14,47 @@ def importProjectList(path):
         os.system("wget -O db/" + name + "/README.md " + rawlink)
     return projectlist
 
+def importTaglist(path):
+    with open(path) as f:
+        taglist = json.load(f)
+    print("Taglist:")
+    print(taglist)
+    return taglist
+
 def extractData(projectlist):
     for project in projectlist:
-        name = project["name"]
+        ##name = project["name"]
         projectlib = open(("db/" + name + "/README.md"), 'r').read()
         tagdict = extractTags(projectlib, taglist)
+        createJson(tagdict, name)
 
 def extractTags(projectlib, taglist):
     tagdict = {}
     for tag in taglist:
-        split1 = projectlib.split("<!--"+tag+"-->")
-        split2 = split1[1].split("<!--/"+tag+"-->")
-        tagdict[tag]=split2[0]
+        try:
+            split1 = projectlib.split("<!--"+tag+"-->")
+            split2 = split1[1].split("<!--/"+tag+"-->")
+            ## tagdict[tag]=split2[0]
+            tagdict[tag]=":heavy_check_mark:"
+        except:
+            tagdict[tag]=":x:"
     print(tagdict)
     return tagdict
 
-#def createJson(dblist):
-
-#def saveJson(dbjson):
-
-#def createMarkdownTable(dbjson):
+def createJson(tagdict, name):
+    os.system("rm -vr db/collect.json")
+    collectjson = []
+    for project in projectlist:
+        name = project["name"]
+        projectlib = open(("db/" + name + "/README.md"), 'r').read()
+        tagdict = extractTags(projectlib, taglist)
+        with open(('db/' + name + '/' + name+'.json'), 'w') as f:
+            json.dump(tagdict, f)
+        collect.append(tagdict)
+    return collect
 
 #def saveMarkdownTable(dbmarkdown):
 
-projectlist = importProjectList("db/project_list.json")
+projectlist = importProjectlist("db/project_list.json")
+taglist = importTaglist("db/tag_list.json")
 extractData(projectlist)
